b48d1488f9858534930af7927289cc82b3030624
cheewengg <cheewengg@gmail.com>
2022-04-09 18:14:33 +0800

    LC: Improve 3Sum

--- leetcode/solutions/3sum.js
+++ leetcode/solutions/3sum.js
@@ -1,7 +1,8 @@
 // https://leetcode.com/problems/3sum/
+// https://leetcode.com/submissions/detail/676881763/
 // Runtime: 499 ms, faster than 22.93% of JavaScript online submissions for 3Sum.
 // Memory Usage: 61.4 MB, less than 16.90% of JavaScript online submissions for 3Sum.
-
+// O(n**2) time, O(n**3) space where n === nums.length
 /**
  * @param {number[]} nums
  * @return {number[][]}
@@ -10,30 +11,33 @@ const threeSum = function (nums) {
   const arrLength = nums.length;
 
   if (arrLength < 3) return [];
-  const numsSorted = [...nums].sort((a, b) => a - b);
-  const res = new Set();
+  nums.sort((a, b) => a - b);
+  const res = [];
 
   for (let i = 0; i < arrLength - 2; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+
     let left = i + 1;
     let right = arrLength - 1;
 
-    const currentNum = numsSorted[i];
+    const currentNum = nums[i];
     const remainder = 0 - currentNum;
 
     while (left < right) {
-      const leftNum = numsSorted[left];
-      const rightNum = numsSorted[right];
+      const leftNum = nums[left];
+      const rightNum = nums[right];
 
       const sum = leftNum + rightNum;
       if (sum === remainder) {
-        res.add(`${currentNum},${leftNum},${rightNum}`);
+        res.push([currentNum, leftNum, rightNum]);
         left++;
         right--;
+        while (nums[left] === leftNum) left++;
+        while (nums[right] === rightNum) right--;
       } else if (sum < remainder) left++;
       else right--;
     }
   }
-  return [...res].map((str) =>
-    str.split(",").map((numStr) => parseInt(numStr))
-  );
+
+  return res;
 };
